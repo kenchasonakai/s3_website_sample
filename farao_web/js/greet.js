0273370d75c1d20f2af469c3cf4534e9c0e9808c
kenchaso <kenchasonakai@gmail.com>
2023-07-13 15:25:53 +0900

    no such keyエラーの再現

--- farao_web/js/greet.js
+++
@@ -1,8 +0,0 @@
-window.addEventListener('DOMContentLoaded', function() {
-  const greetButton = document.getElementById('greetButton');
-  const greetInput = document.getElementById('greetInput');
-  const greet = document.getElementById('greet');
-  greetButton.addEventListener('click', function() {
-    greet.innerHTML = greetInput.value + 'さん、こんにちは！';
-  });
-})
